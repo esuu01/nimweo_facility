78b242b50d48e094e1c5d9036a8d0b5a51aca6ac
Maciej Mierzejewski <maciej.mierzejewski.01@gmail.com>
2024-03-22 21:30:28 +0100

    ez

--- screens/products/Home.js
+++ screens/products/Home.js
@@ -1,7 +1,6 @@
 import {TitleHeader} from "../../components/Headers";
 import {AppLayout} from "../../layouts/_app";
 
-import {Camera, CameraType } from "expo-camera";
 import { useState } from 'react';
 import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
